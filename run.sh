c6fbc6d96d930985ed75ec335d38e42865bf24fc
Mnaim03 <99801017+Mnaim03@users.noreply.github.com>
2024-10-26 09:53:09 +0200

    first

---
+++ run.sh
@@ -0,0 +1,16 @@
+cd
+
+#git pull
+
+#iniziallizzo abiente virtuale per avviare Selinium
+python3 -m venv myenv
+
+# Attiva l'ambiente virtuale
+source myenv/bin/activate  # Su Mac e Linux
+
+cd /Users/mohamadnaim/Desktop/IPMN/
+
+chmod +x main.py
+python3 main.py
+
+#git push
