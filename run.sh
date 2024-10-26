f87865abfe5a7121db1cce6dcfe379ca28d4e49b
Mnaim03 <99801017+Mnaim03@users.noreply.github.com>
2024-10-26 18:33:12 +0200

    Update Channels

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 cd
 
 #git pull
+git pull
 
 #iniziallizzo abiente virtuale per avviare Selinium
 python3 -m venv myenv
@@ -8,9 +9,27 @@ python3 -m venv myenv
 # Attiva l'ambiente virtuale
 source myenv/bin/activate  # Su Mac e Linux
 
-cd /Users/mohamadnaim/Documents/tvmn
+
+#analizza OS
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        #Cartella Su Server Linux
+       echo "Server Linux"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+       cd /Users/mohamadnaim/Documents/tvmn
+       echo "Macbook"
+fi
+
+
 
 chmod +x main.py
 python3 main.py
 
 #git push
+git add ./*
+git commit -m 'Update Channels'
+git push
+
+#ricopio mono.m3u8 nella cartella apache
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        #ricopio
+fi
