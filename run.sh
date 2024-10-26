eb0f00a0154fc6f0c06ee280b28690d221ebe8b7
Mnaim03 <99801017+Mnaim03@users.noreply.github.com>
2024-10-26 10:17:45 +0200

    new mono

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ python3 -m venv myenv
 # Attiva l'ambiente virtuale
 source myenv/bin/activate  # Su Mac e Linux
 
-cd /Users/mohamadnaim/Desktop/IPMN/
+cd /Users/mohamadnaim/Documents/tvmn
 
 chmod +x main.py
 python3 main.py
